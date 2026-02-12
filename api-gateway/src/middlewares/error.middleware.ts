2d79c1874b1de8529df191b7a5d8a52fa2363394
Fred Omongole <fomongole091600@gmail.com>
2026-02-12 10:47:59 +0300

    Update error.middleware.ts

--- api-gateway/src/middlewares/error.middleware.ts
+++ api-gateway/src/middlewares/error.middleware.ts
@@ -16,26 +16,23 @@ export const notFound = (req: Request, res: Response, next: NextFunction) => {
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode = err.statusCode || 500;
 
-    //Strictly returning single strings.
     let message: string = err.message || 'Internal Server Error';
 
-    // 1. Handle Zod Errors (Validation)
+    // 1. Handles Zod Errors (Validation)
     if (err instanceof ZodError) {
         statusCode = 400;
-        // We take only the FIRST error message from the issues array.
+        // Taking only the FIRST error message from the issues array.
         message = err.issues[0].message;
     }
 
-    // 2. Handle Mongoose Duplicate Key (e.g., duplicate email registration)
+    // 2. Handles Mongoose Duplicate Key (e.g., duplicate email registration)
     if (err.code === 11000) {
         statusCode = 409;
         const field = Object.keys(err.keyValue || {})[0] || 'input';
 
-        // Return a clean string message
         message = `${field} already exists`;
     }
 
-    // 3. Send Response
     res.status(statusCode).json({
         status: 'fail',
         message
